9b75c409010fb2f4a374b6d1c89b9995ed91b311
Jan Esser <j.esser@optadata-gruppe.de>
2021-06-28 10:17:01 +0200

    es6 modules with innerHTML and runtime-dependency

---
+++ MyGreeterModule.js
@@ -0,0 +1,25 @@
+// https://developers.google.com/web/fundamentals/web-components/shadowdom -->
+
+class MyGreeter extends HTMLElement {
+    constructor() {
+        super();
+
+        const shadow = this.attachShadow({ mode: 'open' })
+        shadow.innerHTML = `
+<style>div { color: red; }</style>
+<div>Hello awesome <slot name="my-name">Unknown</slot>, glad to see ya!</div>`
+
+        this.$myName = this.shadowRoot.querySelector('slot[name="my-name"]');
+    }
+
+    static get observedAttributes() {
+        return ['subject'];
+    }
+
+    attributeChangedCallback(name, oldValue, newValue) {
+        this.$myName.textContent = newValue;
+    }
+
+}
+
+customElements.define('my-greeter', MyGreeter);
